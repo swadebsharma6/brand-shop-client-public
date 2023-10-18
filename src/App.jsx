d734ab1adfbb233dd8f9ecb8cc4c2f04b96d7598
swadebsharma6 <swadebriva2012@gmail.com>
2023-10-18 06:33:43 +0600

    added all files to start the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+     <h3 className='text-center font-bold text-purple-600 my-10'>This site is build from react, react router, firebase, mongodb</h3>
+    </div>
+  )
+}
+
+export default App
